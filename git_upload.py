0ca4d3e1240fbba1646e2fc89fa50bcc3d347a9d
rishabhdubeyiisc <rishidd7devx2@gmail.com>
2021-03-13 19:44:35 +0530

    server working

--- git_upload.py
+++ git_upload.py
@@ -5,6 +5,8 @@ from utils import run_cmd
 
 def usage():
     print("please add up commit msg")
+    print("To set config    : pass True as second argument")
+    print("example          : 'This is my commit msg' 'True'")
 
 
 if (len(sys.argv) < 2 ):
@@ -17,8 +19,10 @@ commit_msg = sys.argv[1]
 print("committing : {} ".format(commit_msg))
 commit_msg = "'"+ str(commit_msg) +"'" 
 
-run_cmd('git config --global user.email "rishidd7devx2@gmail.com"')
-run_cmd('git config --global user.name "rishabhdubeyiisc"')
+if (len(sys.argv) > 2):
+    if (sys.argv[2] == 'True'):
+        run_cmd('git config --global user.email "rishidd7devx2@gmail.com"')
+        run_cmd('git config --global user.name "rishabhdubeyiisc"')
 
 run_cmd("rm -r *.log")
 run_cmd("git add .")
