ed33024fdd9c4442dead8fc233cfd4e37155f040
rishabhdubeyiisc <rishidd7devx2@gmail.com>
2021-03-15 15:49:31 +0530

    data frame

--- git_push.py
+++ git_push.py
@@ -24,6 +24,7 @@ print("committing : {} ".format(commit_msg))
 commit_msg = "'"+ str(commit_msg) +"'" 
 
 run_cmd("rm -r *.log")
+run_cmd("rm -rf '__pycache__'")
 run_cmd("git add .")
 run_cmd("git commit -m " + str(commit_msg) )
 run_cmd("git push")
